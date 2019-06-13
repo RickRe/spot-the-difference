5014258c0309f4aa46e1aa47f5122a492cebff36
BigBear <39886805+RickRe@users.noreply.github.com>
2019-06-13 16:06:06 +0100

    Add code comments
    Add code comments. 
    This spot-the-difference can be used to compare two identical digital images with update in one of the two images.

--- src/pythonimgdiff.py
+++ src/pythonimgdiff.py
@@ -3,10 +3,23 @@ from PIL import Image, ImageChops
 point_table = ([0] + ([255] * 255))
 
 def black_or_b(a, b):
+    # Returns the absolute value of the pixel-by-pixel difference between the two images.
+    # pixel value of diff (image object): if no difference, the value is 0, otherwise the abs(difference).
+    # https://pillow.readthedocs.io/en/3.0.x/reference/ImageChops.html
     diff = ImageChops.difference(a, b)
     diff = diff.convert('L')
+    
+    # Maps the diff (image) to 0 or 255. If a pixel of diff is 0 (no difference), then the result of it is 0, otherwise is 255.
+    # https://pillow.readthedocs.io/en/3.0.x/reference/Image.html#PIL.Image.Image.point
     diff = diff.point(point_table)
+    
+    # 1-channel image to 3-channels image
+    # https://pillow.readthedocs.io/en/3.0.x/reference/Image.html#PIL.Image.Image.convert
     new = diff.convert('RGB')
+    
+    # Pastes another image b into this image new according to the mask.
+    # Where pixel value of the mask is 255, pixel value of new is replaced by value of b. 
+    # https://pillow.readthedocs.io/en/3.0.x/reference/Image.html#PIL.Image.Image.paste
     new.paste(b, mask=diff)
     return new
 
